abce5292fc4801afefeb17fa2400467bac1cab40
Yoshida Shin <wbcchsyn@gmail.com>
2020-06-18 12:18:18 +0900

    Quit to install racer in rust playbook
    These days "Rust Language Server" is common.

--- playbooks/roles/devel/rust/files/.emacs.d/init.d/rust.el
+++ playbooks/roles/devel/rust/files/.emacs.d/init.d/rust.el
@@ -2,15 +2,9 @@
 (el-get-bundle flycheck)
 (el-get-bundle flycheck-rust)
 (el-get-bundle company)
-(el-get-bundle racer)
 
 (add-hook 'rust-mode-hook 'flycheck-mode)
 (add-to-list 'auto-mode-alist '("\\.rs$" . rust-mode))
-
-(add-hook 'rust-mode-hook 'racer-mode)
-(add-hook 'racer-mode-hook 'eldoc-mode)
-
-(add-hook 'racer-mode-hook 'company-mode)
 
 (require 'rust-mode)
 (define-key rust-mode-map (kbd "TAB") 'company-indent-or-complete-common)
